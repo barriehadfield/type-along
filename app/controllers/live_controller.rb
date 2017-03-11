4cc8f02ebd798760c2b4695ee266b8375fdc919c
Barrie Hadfield <barrie@workshare.com>
2017-03-11 16:18:46 +0000

    all working

--- app/controllers/live_controller.rb
+++ app/controllers/live_controller.rb
@@ -1,6 +1,6 @@
 class LiveController < ApplicationController
   def page
     # render_component
-    render inline: "<%= react_component '::Page', @rails_react_variables, { prerender: true} %>", layout: 'application'
+    # render inline: "<%= react_component '::Page', @rails_react_variables, { prerender: true} %>", layout: 'application'
   end
 end
